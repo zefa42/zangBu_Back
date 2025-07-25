1116b56a6b80fd6bf10f722e8c1f56c4858ae4ab
zefa42 <youngoh07015@gmail.com>
2025-07-25 14:35:33 +0900

    feat: review 상세보기 기능 mapper 구현

--- src/main/java/bjs/zangbu/review/mapper/ReviewMapper.java
+++ src/main/java/bjs/zangbu/review/mapper/ReviewMapper.java
@@ -1,5 +1,6 @@
 package bjs.zangbu.review.mapper;
 
+import bjs.zangbu.review.dto.response.ReviewDetailResponse;
 import bjs.zangbu.review.dto.response.ReviewListResponse;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -8,10 +9,17 @@ import java.util.List;
 
 @Mapper
 public interface ReviewMapper {
+
+    // 리뷰 목록 불러오는 기능
     List<ReviewListResponse> selectByBuilding(@Param("buildingId") long buildingId);
 
     Long countByBuilding(@Param("building") Long buildingId);
 
     // 최신 리뷰의 별점 가져오기
     Integer selectLatestReviewRank(@Param("buildingId") Long buildingId);
+
+
+
+    // 리뷰 상세보기 기능
+    ReviewDetailResponse selectById(@Param("reviewId") Long reviewId);
 }
